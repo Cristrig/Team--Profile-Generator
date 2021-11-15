0d581dd23bf63b912d13c7a9725af18fe537f5b6
Cristian Trigueros <cristiantrigueros@Cristians-MBP.lan>
2021-11-15 12:06:12 -0600

    initial commit

--- Lib/Engineer.js
+++ Lib/Engineer.js
@@ -1,15 +1,19 @@
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
 const Employee = require("./Employee");
 
 class Engineer extends Employee {
-  constructor(name, id, email, github) {
-    super(name, id, email);
+  constructor(id, name, email, github) {
+    super(id, name, email);
     this.github = github;
   }
-  getRole() {
-    return "Engineer";
-  }
+
   getGithub() {
     return this.github;
+  }
+
+  getRole() {
+    return "Engineer";
   }
 }
 
